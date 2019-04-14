209335ffc58aa7a84128fb8daf1a3c9b44c27abd
Marcus Wiseman <marcus.e.wiseman@gmail.com>
2019-04-14 16:05:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ var recipes = {
   cereal: ["milk", "cherrios"], 
   soup: ["chicken", "chicken broth"], 
   frenchToast: ["bread", "butter", "eggs"]
-}
+};
+
+function updateObjectWithKeyAndValue(obj, key, value) {
+  
+};
