89102e63ede0153b7a0d96501a8c3ebd9183dafb
Marcus Wiseman <marcus.e.wiseman@gmail.com>
2019-04-14 16:13:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var recipes = {
 };
 
 function updateObjectWithKeyAndValue(obj, key, value) {
- obj;
- return Object.assign({obj}, {key: value});
+ return (
+   
+   Object.assign({obj}, {key: value});
+   )
+ Object.assign({obj}, {key: value});
 }
